1af94d52743233b6a386d0c3b62bea4c1ab02a1d
unknown <231-001278@IMEPACAD.local>
2024-03-20 20:30:51 -0300

    primeiro commit, nao funcional

---
+++ src/main/java/br/edu/imepac/servicos/AlunoService.java
@@ -0,0 +1,24 @@
+package br.edu.imepac.servicos;
+
+import br.edu.imepac.entidades.Aluno;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class AlunoService {
+    public Aluno cadastrar(Aluno novoAluno){
+
+    };
+
+    public List<Array> lerTodos(){
+
+    };
+
+    public Aluno deletar(long id){
+
+    };
+
+    public Aluno atualizar(Aluno alunoAtualizado){
+
+    };
+}
